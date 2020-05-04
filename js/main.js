58f8d098e918a71b75d68b152bfcee0bf709e5b9
Fie Johannsen <fiejohannsen@live.dk>
2020-05-04 22:19:37 +0200

    animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+"use strict";
+
+function showLoader(show) {
+  let loader = document.querySelector('#loader');
+  if (show) {
+    loader.classList.remove("hide");
+  } else {
+    loader.classList.add("hide");
+  }
+}
+
 function navIcon(x) {
   x.classList.toggle("change");
   var x = document.getElementById("navContainer");
@@ -7,6 +18,10 @@ if (x.style.display === "block") {
   x.style.display = "block";
 }
 }
+
+
+
+// Sliders
 let aboutSlideIndex = 1;
 showAboutSlides(aboutSlideIndex);
 
@@ -67,4 +82,51 @@ function showPartnerSlides(n) {
   }
   partnerSlides[partnerSlideIndex - 1].style.display = "flex";
   partnerDots[partnerSlideIndex - 1].className += " active";
+}
+
+// Animation on scroll
+(function() {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('fade-in-element');
+        element.classList.remove('hidden');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+})();
+
+
+// Show more buttons
+function showMore(info) {
+  var dots = document.querySelector(`.aboutSlides[data-info="${info}"] .dots`);
+  var moreText = document.querySelector(`.aboutSlides[data-info="${info}"] .more`);
+  var btnText = document.querySelector(`.aboutSlides[data-info="${info}"] .moreBtn`);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Læs mere";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Læs mindre";
+    moreText.style.display = "inline";
+  }
 }
